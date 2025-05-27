c29c0e82fc930802b100f298e8553f1d262c9261
jil1213 <lawine@tirol.gv.at>
2025-05-27 17:39:20 +0200

    plot profiles as grain colors

--- readjson.py
+++ readjson.py
@@ -2,6 +2,9 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import profiles_parameters #also correct? 
+from profiles_parameters import LABELS, LABELS_LONG, COLORS #right? 
+
 # load .json snowprofiles from LAWIS
 def load_lawis_profile(json_path):
     with open(json_path, "r", encoding="utf-8") as f:
@@ -74,15 +77,43 @@ def plot_lawis_hardness(df, name="LAWIS_Profile"):
     plt.tight_layout()
     plt.show()
 
+
+def plot_lawis_colored_grain(df, name="LAWIS_Profile"):
+    reverse_labels_long = {v.lower(): k for k, v in LABELS_LONG.items()}
+
+    fig, ax = plt.subplots(figsize=(10, 2))
+
+    for i in range(0, len(df), 2):
+        left = df.iloc[i + 1]["distance"]
+        right = df.iloc[i]["distance"]
+        grain1_text = df.iloc[i]["grain1"].lower().strip()
+
+        # Übersetze grain1 zu label_id über den LANGTEXT
+        label_id = reverse_labels_long.get(grain1_text, 0)  # default zu 0 ("not_labelled")
+        color = COLORS.get(label_id, "dimgray")
+
+        ax.fill_betweenx([0, 1], left, right, color=color)
+
+    ax.set_ylim(0, 1)
+    ax.set_xlim(df["distance"].min(), df["distance"].max())
+    ax.set_xlabel("Distance from surface (mm)")
+    ax.set_yticks([])
+    ax.set_title(name)
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
     #load profile day 1
     df_day1 = load_lawis_profile("data/LawisProfile23044.json")
     df_day1 = convert_to_snowdepth(df_day1)
     print(df_day1)
-    plot_lawis_hardness(df_day1, name="LAWIS Profile Day 1")
+    #plot_lawis_hardness(df_day1, name="LAWIS Profile Day 1")
 
     #load profile day 2
     df_day2 = load_lawis_profile("data/LawisProfile23778.json")
     df_day2 = convert_to_snowdepth(df_day2)
     print(df_day2)
-    plot_lawis_hardness(df_day2, name="LAWIS Profile Day 2")
+    #plot_lawis_hardness(df_day2, name="LAWIS Profile Day 2")
+    plot_lawis_colored_grain(df_day1, name="LAWIS Profile Day 1")
+    plot_lawis_colored_grain(df_day2, name="LAWIS Profile Day 2")
