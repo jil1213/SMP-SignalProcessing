f244f71fe7e6b2be3101fff349a553ac236b6bb5
jil1213 <lawine@tirol.gv.at>
2025-06-03 15:37:59 +0200

    improved plotting style

--- code_json/alignSMPtoJson.py
+++ code_json/alignSMPtoJson.py
@@ -14,8 +14,13 @@ def align_SMP_to_Snowprofile(df):
         #align to second Snowprofile
         json, boundaries = load_lawis_profile("data/LawisProfile23778.json")
 
-    # Plot SMP together with JSON profile
+    # Plot SMP together with JSON profile !!ATTENTION!! plot has fixed height for force (7) 
     fig, ax = plt.subplots(figsize=(8, 5))
+
+    #plot SMP force
+    ax.plot(df["distance"], df["force"], label="SMP Force")
+    ax.set_ylabel("Force (N)")
+    ax.set_ylim(0, 7) # max fix y axis for better comparison
 
     # plot first grain colors as colored bands
     current_color = json["color"].iloc[0]
@@ -25,24 +30,25 @@ def align_SMP_to_Snowprofile(df):
         color = json["color"].iloc[i]
         if color != current_color or i == len(json) - 1:
             end = json["distance"].iloc[i]
-            ax.fill_betweenx([df["force"].min(), df["force"].max()], start, end, color=current_color)
+            ax.fill_betweenx([0, 7], start, end, color=current_color) #!Fixed height y axis
             start = end
             current_color = color
 
-    #plot SMP force
-    ax.plot(df["distance"], df["force"], label="SMP Force")
-    ax.set_ylabel("Force (N)")
+    # Plot boundaries as vertical lines with 80% transparency
+    for boundary in boundaries["distance"]:
+        ax.axvline(x=boundary, color='black', linestyle=':', linewidth=0.8, alpha=0.5, zorder=5)
 
     # create second y axis
     ax2 = ax.twinx()
     # plot Hardness Index of JSON profile
-    ax2.plot(json["distance"], json["hardness_id"], color='black', linestyle='--', label="Hardness Index")
+    ax2.plot(json["distance"], json["hardness_id"], color='black', linestyle='--', linewidth=0.8, alpha=0.5, label="Hardness Index")
     ax2.set_ylabel("Hardness Index")
     ax2.set_ylim(bottom=0)
 
-    ax.grid()
+    ax2.grid()
+    ax2.legend()
     ax.set_xlabel("Distance from surface (mm)")
-    ax.set_title(f"{name} aligned to manual profile day {df.attrs['date']}")
+    ax.set_title(f"{name} and manual profile day {df.attrs['date']}")
     plt.tight_layout()
     plt.show()
 
