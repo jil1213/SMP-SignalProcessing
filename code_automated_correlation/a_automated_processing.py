9924f1bc15875ad67fa7c3831975a839a12e8186
Jil <jillehnert@gmail.com>
2026-09-17 15:56:04 +0200

    test commit

--- code_automated_correlation/a_automated_processing.py
+++ code_automated_correlation/a_automated_processing.py
@@ -7,7 +7,7 @@ from itertools import combinations
 from snowmicropyn import Profile
 from scipy.signal import correlate
 
-from code_automated_correlation.new_surface_detection import detect_surface # my own method to detect surface
+from code_automated_correlation.new_surface_detection import detect_surface # my own method to detect surface 
 
 
 def load_profiles(folder_path):
